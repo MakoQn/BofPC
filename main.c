b85186425ccf3387177a856f73b8394012e925c4
MakoQn <borisovka.club@gmail.com>
2024-02-24 20:54:09 +0300

    test

--- main.c
+++ main.c
@@ -5,9 +5,29 @@
 # include "E:\C23Exe\libs\data_structures\ordered_set\ordered_set.h"
 # include "E:\C23Exe\libs\data_structures\vector\vector.h"
 
+void test_pushBack_emptyVector(){
+    vector v = createVector(0);
+
+    pushBack(&v, 5);
+    assert(getVectorValue(&v, 0) == 5);
+}
+
+void test_pushBack_fullVector(){
+    vector v = createVector(2);
+
+    pushBack(&v, 1);
+    pushBack(&v, 2);
+    pushBack(&v, 3);
+
+    assert(getVectorValue(&v, 2) == 3);
+}
+
+void test(){
+    test_pushBack_emptyVector();
+    test_pushBack_fullVector();
+}
 
 int main() {
-    vector v = createVector(5);
 
     return 0;
 }
