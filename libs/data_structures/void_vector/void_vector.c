dd7ab86b6d86bdf307d848340a8f3ce529e5f9e1
MakoQn <borisovka.club@gmail.com>
2024-02-25 15:58:13 +0300

    memory usage of vectorVoid

--- libs/data_structures/void_vector/void_vector.c
+++ libs/data_structures/void_vector/void_vector.c
@@ -2,6 +2,9 @@
 #define INC_VOID_VECTOR_H
 
 #include <limits.h>
+# include <stdint.h>
+# include <malloc.h>
+# include <stdio.h>
 #include "void_vector.h"
 
 typedef struct vectorVoid {
@@ -14,5 +17,88 @@ typedef struct vectorVoid {
                         // если вектор хранит float -
                         // то поле baseTypeSize = sizeof(float)
 } vectorVoid;
+
+//возвращает пустой вектор вместимостью n и размером базового типа baseTypeSize
+vectorVoid createVectorV(size_t n, size_t baseTypeSize){
+    if (n == 0)
+        return (vectorVoid) {
+                NULL,
+                0,
+                n,
+                baseTypeSize
+        };
+    else{
+        vectorVoid *a = malloc(sizeof(baseTypeSize) * n);
+
+        if (a == NULL){
+            free(a);
+
+            fprintf(stderr, "bad alloc");
+            exit(1);
+        }else{
+            free(a);
+
+            return (vectorVoid) {
+                    malloc(sizeof(baseTypeSize) * n),
+                    0,
+                    n,
+                    baseTypeSize
+            };
+        }
+    }
+}
+
+//изменяет количество памяти выделенное
+//под хранение элементов вектора
+void reserveV(vectorVoid *v, size_t newCapacity){
+    if (newCapacity == v->capacity)
+        return;
+
+    if (newCapacity == 0){
+        v->data = NULL;
+        v->size = 0;
+        v->capacity = 0;
+        v->baseTypeSize = 0;
+
+        free(v->data);
+
+        return;
+    }else if (newCapacity < v->capacity){
+        v->size = newCapacity;
+        v->capacity = newCapacity;
+    }else{
+        vectorVoid *a = malloc(sizeof(v->baseTypeSize) * (newCapacity - v->capacity));
+
+        if (a == NULL){
+            free(a);
+
+            fprintf(stderr, "bad alloc");
+            exit(1);
+        }else {
+            free(a);
+
+            v->capacity = newCapacity;
+        }
+    }
+
+    v->data = realloc(v->data, sizeof(v->baseTypeSize) * newCapacity);
+}
+
+//освобождает память, выделенную под
+//неиспользуемые элементы
+void shrinkToFitV(vectorVoid *v){
+    reserveV(v, v->size);
+}
+
+//удаляет элементы из контейнера
+//но не освобождает выделенную память
+void clearV(vectorVoid *v){
+    *v = createVectorV(v->capacity, v->baseTypeSize);
+}
+
+//освобождает память, выделенную вектору
+void deleteVectorV(vectorVoid *v){
+    reserveV(v, 0);
+}
 
 # endif
--- libs/data_structures/void_vector/void_vector.h
+++ libs/data_structures/void_vector/void_vector.h
@@ -2,6 +2,9 @@
 #define INC_VOID_VECTOR_H
 
 #include <limits.h>
+# include <stdint.h>
+# include <malloc.h>
+# include <stdio.h>
 
 typedef struct vectorVoid {
     void *data; // указатель на нулевой элемент вектора
@@ -13,5 +16,23 @@ typedef struct vectorVoid {
                         // если вектор хранит float -
                         // то поле baseTypeSize = sizeof(float)
 } vectorVoid;
+
+//возвращает пустой вектор вместимостью n и размером базового типа baseTypeSize
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+//изменяет количество памяти выделенное
+//под хранение элементов вектора
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+//освобождает память, выделенную под
+//неиспользуемые элементы
+void shrinkToFitV(vectorVoid *v);
+
+//удаляет элементы из контейнера
+//но не освобождает выделенную память
+void clearV(vectorVoid *v);
+
+//освобождает память, выделенную вектору
+void deleteVectorV(vectorVoid *v);
 
 # endif
