76925363aad733df142e7c22f93f090be786caea
MakoQn <borisovka.club@gmail.com>
2024-03-16 14:53:07 +0300

    rows and columns

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -93,4 +93,22 @@ void outputMatrices(matrix *ms, int nMatrices){
     }
 }
 
+//обмен строк с порядковыми номерами i1 и i2 в матрице m.
+void swapRows(matrix m, int i1, int i2){
+    int temp = *m.values[i1];
+
+    *m.values[i1] = *m.values[i2];
+    *m.values[i2] = temp;
+}
+
+//обмен колонок с порядковыми номерами j1 и j2 в матрице m.
+void swapColumns(matrix m, int j1, int j2){
+    for (int i = 0; i < m.nRows; i++){
+        int temp = m.values[i][j1];
+
+        m.values[i][j1] = m.values[i][j2];
+        m.values[i][j2] = temp;
+    }
+}
+
 # endif
