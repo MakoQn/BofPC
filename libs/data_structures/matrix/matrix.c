e4726ac38f8b5e5af1be49a3e4da13f8f6b84fd2
MakoQn <borisovka.club@gmail.com>
2024-03-16 14:11:24 +0300

    input output matrix

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -4,6 +4,7 @@
 #include "matrix.h"
 # include <malloc.h>
 # include <stdint.h>
+# include <stdio.h>
 
 typedef struct matrix {
     int **values; // элементы матрицы
@@ -19,16 +20,20 @@ typedef struct position {
 //размещает в динамической памяти матрицу размером nRows на nCols.
 matrix getMemMatrix(int nRows, int nCols){
     int **values = (int **) malloc(sizeof(int*) * nRows);
+
     for (int i = 0; i < nRows; i++)
         values[i] = (int *) malloc(sizeof(int) * nCols);
+
     return (matrix){values, nRows, nCols};
 }
 
 //размещает в динамической памяти массив из nMatrices матриц размером nRows на nCols.
 matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols){
     matrix *ms = (matrix*) malloc(sizeof(matrix) * nMatrices);
+
     for (int i = 0; i < nMatrices; i++)
         ms[i] = getMemMatrix(nRows, nCols);
+
     return ms;
 }
 
@@ -39,7 +44,7 @@ void freeMemMatrix(matrix *m){
 
     free(m->values);
 
-    m->values == NULL;
+    m->values = NULL;
     m->nRows = 0;
     m->nCols = 0;
 }
@@ -48,6 +53,44 @@ void freeMemMatrix(matrix *m){
 void freeMemMatrices(matrix *ms, int nMatrices){
     for (int i = 0; i < nMatrices; i++)
         freeMemMatrix(&ms[i]);
+}
+
+//ввод матрицы m.
+void inputMatrix(matrix *m){
+    for (int i = 0; i < m->nRows;i++){
+        printf("%d row\n", i + 1);
+
+        for (int j = 0; j < m->nCols;i++)
+            scanf("%d", &m->values[i][j]);
+    }
+
+    printf("\n");
+}
+
+//ввод массива из nMatrices матриц, хранящейся по адресу ms.
+void inputMatrices(matrix *ms, int nMatrices){
+    for (int i = 0;i < nMatrices; i++) {
+        printf("%d matrix\n", i + 1);
+        inputMatrix(&ms[i]);
+    }
+}
+
+//вывод матрицы m.
+void outputMatrix(matrix m){
+    for (int i = 0; i < m.nRows;i++) {
+        for (int j = 0; j < m.nCols; j++)
+            printf("%d ", m.values[i][j]);
+
+        printf("\n");
+    }
+}
+
+// вывод массива из nMatrices матриц, хранящейся по адресу ms.
+void outputMatrices(matrix *ms, int nMatrices){
+    for (int i = 0; i < nMatrices; i++) {
+        printf("%d matrix\n", i + 1);
+        outputMatrix(ms[i]);
+    }
 }
 
 # endif
