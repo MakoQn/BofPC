eb4357c2ea4b1518b7be26cb973d42be186739b2
MakoQn <borisovka.club@gmail.com>
2024-03-17 16:15:32 +0300

    creates Matrix From Array

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -292,4 +292,30 @@ position getMaxValuePos(matrix m){
     return pos;
 }
 
+//возвращает матрицу размера nRows на nCols, построенную из элементов массива a
+matrix createMatrixFromArray(const int *a, size_t nRows, size_t nCols){
+    matrix m = getMemMatrix(nRows, nCols);
+    int k = 0;
+
+    for (int i = 0; i < nRows; i++)
+        for (int j = 0; j < nCols; j++)
+            m.values[i][j] = a[k++];
+
+    return m;
+}
+
+//возвращает указатель на нулевую матрицу массива из nMatrices матриц, размещенных
+//в динамической памяти, построенных из элементов массива a
+matrix *createArrayOfMatrixFromArray(const int *values, size_t nMatrices, size_t nRows, size_t nCols){
+    matrix *ms = getMemArrayOfMatrices(nMatrices, nRows, nCols);
+    int l = 0;
+
+    for (size_t k = 0; k < nMatrices; k++)
+        for (size_t i = 0; i < nRows; i++)
+            for (size_t j = 0; j < nCols; j++)
+                ms[k].values[i][j] = values[l++];
+
+    return ms;
+}
+
 # endif
