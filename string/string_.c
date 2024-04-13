b077d5da7136733a07748b3a0d100eded2f3d9e3
MakoQn <borisovka.club@gmail.com>
2024-04-13 15:15:23 +0300

    task 3 strlen_

--- string/string_.c
+++ string/string_.c
@@ -32,9 +32,42 @@ void test_findLength(){
     test_findLength_notZeroLength();
 }
 
+//количество символов в строке
+size_t strlen_(const char *begin) {
+    char *end = begin;
+    while (*end != '\0')
+        end++;
+    return end - begin;
+}
+
+void test_strlen__zeroLength(){
+    char s[] = "";
+
+    assert(strlen_(s) == 0);
+}
+
+void test_strlen__noSymbols(){
+    char s[5] = "";
+
+    assert(strlen_(s) == 0);
+}
+
+void test_strlen__notNoSymbols(){
+    char s[10] = "Hello";
+
+    assert(strlen_(s) == 5);
+}
+
+void test_strlen_(){
+    test_strlen__zeroLength();
+    test_strlen__noSymbols();
+    test_strlen__notNoSymbols();
+}
+
 //тестирует функции, написанные выше
 void test(){
     test_findLength();
+    test_strlen_();
 }
 
 # endif
--- string/string_.h
+++ string/string_.h
@@ -4,6 +4,9 @@
 //нахождение длины строки
 int findLength(const char *str);
 
+//количество символов в строке
+size_t strlen_(const char *begin);
+
 //тестирует функции, написанные выше
 void test();
 
